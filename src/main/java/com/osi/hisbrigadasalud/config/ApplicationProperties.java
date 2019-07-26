a48c1aba4053f9011948982f1627da5a2a1f87f5
fachavarro <51423682+fachavarro@users.noreply.github.com>
2019-07-26 11:41:19 -0500

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/osi/hisbrigadasalud/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.osi.hisbrigadasalud.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to His Brigada Salud.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/com/osi/hisbrigadasalud/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.osi.hisbrigadasalud.service.dto;
